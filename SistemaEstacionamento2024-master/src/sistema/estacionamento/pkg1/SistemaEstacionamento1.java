a8d35ad6f76670a86288faff9e5cae8ed7d83996
RobertaBS09 <robertabarbosas123@gmail.com>
2024-06-18 15:59:06 -0300

    Create SistemaEstacionamento1.java

---
+++ SistemaEstacionamento2024-master/src/sistema/estacionamento/pkg1/SistemaEstacionamento1.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package sistema.estacionamento.pkg1;
+
+/**
+ *
+ * @author rober
+ */
+public class SistemaEstacionamento1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        new talaLogin().setVisible(true);
+    }
+    
+}
